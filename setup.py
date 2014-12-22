8a871cf9924325e2b16c485d52b6b6603bf0ef06
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-22 16:34:25 +0000

    added base python skeleton

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import time
+_version = '0.1.0.%s' % int(time.time())
+
+_packages = find_packages(where='.')
+
+install_requires = [
+    'python-forecastio',
+    'celery',
+]
+
+setup(
+    name='green-catalyst',
+    version=_version,
+    description='An app to boost your plants',
+    author='Joao Figueiredo',
+    author_email='jfigueiredo@egoscape.net',
+    url='log.egoscape.net',
+    install_requires=install_requires,
+    packages=_packages,
+    include_package_data=True,
+)

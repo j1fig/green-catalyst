e4c49d6d234e1fc47601174c397391e473877377
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-24 17:36:13 +0000

    added pigpio setup steps

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -5,5 +5,15 @@ apt-get update
 apt-get install -y build-essential python-dev python-pip git
 pip install virtualenv
 
+# virtenv setup
 virtualenv .gc
 source .gc/bin/activate
+
+# pigpio install
+wget abyz.co.uk/rpi/pigpio/pigpio.zip
+unzip pigpio.zip
+cd PIGPIO
+make
+make install
+cd ..
+rm -rf PIGPIO
